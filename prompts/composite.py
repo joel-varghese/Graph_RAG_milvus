c53ef8efac858d1185246e18caf8941aba31405a
joel-varghese <joelvar17@outlook.com>
2026-04-14 09:11:10 -0400

    Refactoring with composite RAG Impl

---
+++ prompts/composite.py
@@ -0,0 +1,382 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_groq import ChatGroq
+from langchain.schema import Document
+from langchain_community.graphs import Neo4jGraph
+from langchain_classic.chains import GraphCypherQAChain
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.output_parsers import StrOutputParser
+from langgraph.graph import END, StateGraph
+from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv, find_dotenv
+from pymilvus import MilvusClient
+from typing_extensions import TypedDict
+from typing import List
+
+import os
+_ = load_dotenv(find_dotenv())
+
+
+LLM_NAME = "openai/gpt-oss-120b"
+ZILLIZ_TOKEN     = os.environ["ZILLIZ_API_KEY"]
+CLUSTER_ENDPOINT = "https://in03-5223ff782a72af1.serverless.aws-eu-central-1.cloud.zilliz.com"
+COLLECTION_NAME  = "graph_rag_arxiv"
+api = os.getenv("GROQ_API_KEY")
+
+
+prompt = PromptTemplate(
+    template="""You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context from a vector store and a graph database to answer the question. If you don't know the answer, just say that you don't know. 
+    Use three sentences maximum and keep the answer concise:
+    Question: {question} 
+    Vector Context: {context} 
+    Graph Context: {graph_context}
+    Answer: 
+    """,
+    input_variables=["question", "context", "graph_context"],
+)
+
+composite_chain = prompt | llm | StrOutputParser()
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+        graph_context: results from graph search
+    """
+
+    question: str
+    generation: str
+    web_search: str
+    documents: List[str]
+    graph_context: str
+
+def retrieve(state):
+    """
+    Retrieve documents from vectorstore
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    # Retrieval
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
+
+def generate(state):
+    """
+    Generate answer using RAG on retrieved documents and graph context
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, generation, that contains LLM generation
+    """
+    print("---GENERATE---")
+    question = state["question"]
+    documents = state.get("documents", [])
+    graph_context = state.get("graph_context", "")
+
+    # Composite RAG generation
+    generation = composite_chain.invoke(
+        {"question": question, "context": documents, "graph_context": graph_context}
+    )
+    return {
+        "documents": documents,
+        "question": question,
+        "generation": generation,
+        "graph_context": graph_context,
+    }
+
+def grade_documents(state):
+    """
+    Determines whether the retrieved documents are relevant to the question
+    If any document is not relevant, we will set a flag to run web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Filtered out irrelevant documents and updated web_search state
+    """
+
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # Score each doc
+    filtered_docs = []
+    web_search = "No"
+    for d in documents:
+        score = retrieval_grader.invoke(
+            {"question": question, "document": d.page_content}
+        )
+        grade = score["score"]
+        # Document relevant
+        if grade.lower() == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(d)
+        # Document not relevant
+        else:
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            # We do not include the document in filtered_docs
+            # We set a flag to indicate that we want to run web search
+            web_search = "Yes"
+            continue
+    return {"documents": filtered_docs, "question": question, "web_search": web_search}
+
+
+def web_search(state):
+    """
+    Web search based on the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Appended web results to documents
+    """
+
+    print("---WEB SEARCH---")
+    question = state["question"]
+    documents = state.get("documents", [])  # Use get() with a default empty list
+
+    # Web search
+    docs = web_search_tool.invoke({"query": question})
+    web_results = "\n".join([d["content"] for d in docs])
+    web_results = Document(page_content=web_results)
+    documents.append(web_results)
+
+    return {"documents": documents, "question": question}
+
+### Conditional edge
+def route_question(state):
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    print(question)
+    source = question_router.invoke({"question": question})
+    print(source)
+    print(source["datasource"])
+
+    if source["datasource"] == "graphrag":
+        print("---TRYING GRAPH SEARCH---")
+        graph_result = graph_search({"question": question})
+        if graph_result["graph_context"] != "No results found in the graph database.":
+            return "graphrag"
+        else:
+            print("---NO RESULTS IN GRAPH, FALLING BACK TO VECTORSTORE---")
+            return "retrieve"
+    elif source["datasource"] == "vectorstore":
+        print("---ROUTE QUESTION TO VECTORSTORE RAG---")
+        return "retrieve"
+    elif source["datasource"] == "web_search":
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return "websearch"
+    
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or add web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+
+    print("---ASSESS GRADED DOCUMENTS---")
+    question = state["question"]
+    web_search = state["web_search"]
+    filtered_documents = state["documents"]
+
+    if web_search == "Yes":
+        # All documents have been filtered check_relevance
+        # We will re-generate a new query
+        print(
+            "---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
+        )
+        return "websearch"
+    else:
+        # We have relevant documents, so generate answer
+        print("---DECISION: GENERATE---")
+        return "generate"
+    
+def graph_search(state):
+    """
+    Perform GraphRAG search using Neo4j
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Updated state with graph search results
+    """
+    print("---GRAPH SEARCH---")
+    question = state["question"]
+
+    # Use the graph_rag_chain to perform the search
+    result = graph_rag_chain.invoke({"query": question})
+
+    # Extract the relevant information from the result
+    # Adjust this based on what graph_rag_chain returns
+    graph_context = result.get("result", "")
+
+    # You might want to combine this with existing documents or keep it separate
+    return {"graph_context": graph_context, "question": question}
+
+def grade_generation_v_documents_and_question(state):
+    """
+    Determines whether the generation is grounded in the document and answers question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Decision for next node to call
+    """
+
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+    grade = grade = score.get("score", "").lower()
+
+    # Check hallucination
+    if grade == "yes":
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        # Check question-answering
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        grade = score["score"]
+        if grade == "yes":
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
+
+ 
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("websearch", web_search)  # web search
+workflow.add_node("retrieve", retrieve)  # retrieve
+workflow.add_node("grade_documents", grade_documents)  # grade documents
+workflow.add_node("generate", generate)  # generatae
+workflow.add_node("graphrag", graph_search)
+
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        "websearch": "websearch",
+        "retrieve": "retrieve",
+        "graphrag": "graphrag",
+    },
+)
+
+# Add edges
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_edge("graphrag", "generate")
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "websearch": "websearch",
+        "generate": "generate",
+    },
+)
+workflow.add_edge("websearch", "generate")
+workflow.add_conditional_edges(
+    "generate",
+    grade_generation_v_documents_and_question,
+    {
+        "not supported": "generate",
+        "useful": END,
+        "not useful": "websearch",
+    },
+)
+
+app = workflow.compile()
+
+# Test
+from pprint import pprint
+
+inputs = {"question": "What are the types of Prompt Engineering?"}
+for output in app.stream(inputs):
+    for key, value in output.items():
+        pprint(f"Finished running: {key}:")
+pprint(value["generation"])
+
+# This is for the vector store retrieval and holds vector context 
+
+llm = ChatGroq(
+    api_key = api,
+    model = LLM_NAME,
+    temperature=0.1
+)
+mc = MilvusClient(
+    uri=CLUSTER_ENDPOINT,
+    token=ZILLIZ_TOKEN
+)
+
+model = SentenceTransformer("BAAI/bge-base-en-v1.5")
+
+question = "llm agent memory"
+
+query_vector = model.encode(
+    [question],
+    normalize_embeddings=True
+)[0].tolist()
+
+retrieved_docs = []
+
+results = mc.search(
+    collection_name=COLLECTION_NAME,
+    data=[query_vector],
+    limit=5,
+    output_fields=["text", "source", "title"]
+)
+
+for hits in results:
+    for hit in hits:
+        retrieved_docs.append({
+            "text": hit["entity"]["text"],
+            "title": hit["entity"]["title"],
+            "source": hit["entity"]["source"],
+            "score": hit["distance"]
+        })
+
+question_router = prompt | llm | JsonOutputParser()
+
+vector_context = question_router.invoke({"question": question})
+
+print(vector_context)
+
+# This is for the grpah context retrieval and holds graph content
+
+# graph = Neo4jGraph(
+#     url=os.getenv("NEO4J_URI"),
+#     username=os.getenv("NEO4J_USERNAME"),
+#     password=os.getenv("NEO4J_PASSWORD"),
+#     database="40d4e79a"
+# )
